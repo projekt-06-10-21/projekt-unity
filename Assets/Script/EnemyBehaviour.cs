6ba3e50384a1f7d3005e24a753232f218ae8d395
que2115 <jakub1016@gmail.com>
2021-10-19 08:35:49 +0200

    gotowe bez damage

--- Assets/Script/EnemyBehaviour.cs
+++ Assets/Script/EnemyBehaviour.cs
@@ -7,10 +7,12 @@ public class EnemyBehaviour : MonoBehaviour
     public int hp = 1;
     public float speed = 3;
     GameObject player;
+    GameMaster gm;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        gm = Camera.main.GetComponent<GameMaster>();
     }
 
     // Update is called once per frame
@@ -18,7 +20,16 @@ public class EnemyBehaviour : MonoBehaviour
     {
 
     }
-    
+    private void FixedUpdate()
+    {
+        Vector3 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        transform.position += direction * Time.deltaTime * speed;
+        //Debug.Log(direction.ToString());
+        //Vector2.Lerp(transform.position, player.transform.position, Time.deltaTime);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), Time.deltaTime);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Destroy(collision.collider.gameObject);
